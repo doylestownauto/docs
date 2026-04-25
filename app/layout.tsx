dc7aad89350c372c3231f8cab5a74251939c8f17
doylestownauto <doylestownautorepair@gmail.com>
2026-04-25 11:39:32 -0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,4 @@
 import React from 'react'
 export default function RootLayout({children}: {children: React.ReactNode}) {
-  return (
-    <html lang="en">
-      <body>{children}</body>
-    </html>
-  )
+  return (<html lang="en"><body>{children}</body></html>)
 }
